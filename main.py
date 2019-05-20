b620c822160c6284cd82cc0f77e77181f7d9dac6
konrad <17689319+konradsmolko@users.noreply.github.com>
2019-05-20 23:38:27 +0200

    All finished except for plotting a graph of times.

--- main.py
+++ main.py
@@ -1,50 +1,104 @@
-from math import sin
-from functools import wraps
-from concurrent.futures.thread import ThreadPoolExecutor
-from jacobi import jacobi
-from gauss_seidel import gauss_seidel
+from timeit import default_timer as timer
+from matrix_calc import prepare, jacobi, gauss_seidel, gauss, mul_mat_by_vec
+import simplejson
 
-_DEFAULT_POOL = ThreadPoolExecutor()
-
-
-def mt_wrapper(f, executor=None):
-    """
-    Funkcja zaczerpnięta ze StackOverflow dla
-    uproszczenia kodu przy przyspieszeniu obliczeń.
-    """
-    @wraps(f)
-    def wrap(*args, **kwargs):
-        return (executor or _DEFAULT_POOL).submit(f, *args, **kwargs)
-
-    return wrap
+_debug = False
+_test = False
 
 
-def prepare(n=998, a1=11.0, a2=-1.0, a3=-1.0):
-    matrix = [[0.0]*n]*n
+def policz(n, a1, a2, a3, zad) -> (float, float):
+    matrix, b = prepare(n, a1, a2, a3)
+    cutoff = 10.0 ** -9
 
-    # Mogłoby być bardziej zoptymalizowane...
-    for i in range(n):
-        for j in range(n):
-            if i == j:
-                matrix[i][j] = a1
-            elif i == j - 1 or i == j + 1:
-                matrix[i][j] = a2
-            elif i == j - 2 or i == j + 2:
-                matrix[i][j] = a3
+    if zad is not None:
+        print("Zaczynam zadanie", zad)
+    print("Start Jacobi, n =", n)
+    start = timer()
+    jr, jiters = jacobi(matrix, b, n, cutoff)
+    end = timer()
+    jtime = end - start
 
-    b = [0.0]*n
-    for i in range(n):
-        b[i] = sin(i)
+    print("Start Gauss-Seidel, n =", n)
+    start = timer()
+    gsr, gsiters = gauss_seidel(matrix, b, n, cutoff)
+    end = timer()
+    gstime = end - start
 
-    return matrix, b
+    if _debug:
+        print("Czas dla metody Jacobiego:", jtime)
+        print("Iteracje Jacobi:", jiters)
+        print("jr:", jr)
+    if _test and jr is not None:
+        jtest = mul_mat_by_vec(matrix, jr)
+        print("test jacobi", jtest)
+
+    if _debug:
+        print("Czas dla metody Gaussa-Seidla:", gstime)
+        print("Iteracje Gauss-Seidel:", gsiters)
+        print("gsr:", gsr)
+    if _test and gsr is not None:
+        gstest = mul_mat_by_vec(matrix, gsr)
+        print("test jacobi", gstest)
+
+    return jtime, gstime
+
+
+def zadanie_d(n, a1, a2, a3) -> float:
+    matrix, b = prepare(n, a1, a2, a3)
+
+    if _debug:
+        print("Zaczynam zadanie D")
+    print("Start LU, n =", n)
+    start = timer()
+    lur, error = gauss(matrix, b)
+    end = timer()
+    lutime = end - start
+    if _debug:
+        print("Czas dla metody faktoryzacji LU:", lutime)
+        print("norm(res):", error)
+        print("r:", lur)
+    if _test:
+        lutest = mul_mat_by_vec(matrix, lur)
+        print("test LU", lutest)
+
+    return lutime
+
+
+def zadanie_e(a1, a2, a3):
+    ns = [100, 500, 1000, 2000, 3000]
+    times_jacobi = []
+    times_gs = []
+    times_lu = []
+    for n in ns:
+        print("Zaczynam iterację pętli. n =", n)
+        jtime, gstime = policz(n, a1, a2, a3, None)
+        lutime = zadanie_d(n, a1, a2, a3)
+        print("Czasy:", jtime, gstime, lutime)
+        times_jacobi.append(jtime)
+        times_gs.append(gstime)
+        times_lu.append(lutime)
+
+    return times_jacobi, times_gs, times_lu
 
 
 def main():
     n = 998
-    matrix, b = prepare(n)
-    cutoff = 10.0**-9
-    jr, ji = jacobi(matrix, b, n, cutoff)
-    gsr, gsi = gauss_seidel(matrix, b, n, cutoff)
+    a1 = 11.0
+    a2 = -1.0
+    a3 = -1.0
+    # policz(n, a1, a2, a3, "A i B")
+    # policz(n, 3.0, a2, a3, "C")
+    # zadanie_d(n, 3.0, a2, a3)
+    times_jacobi, times_gs, times_lu = zadanie_e(a1, a2, a3)
+    print(times_jacobi)
+    print(times_gs)
+    print(times_lu)
+    with open("jacobi.txt", 'w') as file:
+        simplejson.dump(times_jacobi, file)
+    with open("gs.txt", 'w') as file:
+        simplejson.dump(times_gs, file)
+    with open("lu.txt", 'w') as file:
+        simplejson.dump(times_lu, file)
 
 
 if __name__ == "__main__":
--- matrix_calc.py
+++ matrix_calc.py
@@ -1,13 +1,192 @@
-def mul(a, b):
-    # TODO
-    return 1
+from math import sqrt, sin
 
 
-def sub(a, b):
-    # TODO
-    return 1
+def mul_mat_by_mat(a, b):
+    # ma, na = len(a), len(a[0])
+    # mb, nb = len(b), len(b[0])
+    #
+    # if na != mb:
+    #     raise ValueError("Macierze nie mogą być przemnożone!")
+    #
+    # ret = [[0.0 for _ in range(nb)] for _ in range(ma)]
+    # # ret = []
+    # # for i in range(ma):
+    # #     row = [0.0] * nb
+    # #     ret.append(row)
+    #
+    # for i in range(ma):
+    #     for j in range(nb):
+    #         for k in range(mb):
+    #             ret[i][j] += a[i][j] * b[k][j]
+    #
+    # return ret
+    # # podejrzewam że powyższa metoda źle działała
+    return [[sum(el_m * el_n for el_m, el_n in zip(row_m, col_n)) for col_n in zip(*b)] for row_m in a]
 
 
-def norm_res(matrix, n, r, b):
-    # TODO
-    return 1
+def pivot_mat(matrix):
+    #  Metoda zaczerpnięta z internetu.
+    #  https://www.quantstart.com/articles/LU-Decomposition-in-Python-and-NumPy
+    m = len(matrix)
+    id_mat = [[float(i == j) for i in range(m)] for j in range(m)]
+    for j in range(m):
+        row = max(range(j, m), key=lambda i: abs(matrix[i][j]))
+        if j != row:
+            id_mat[j], id_mat[row] = id_mat[row], id_mat[j]
+
+    return id_mat
+
+
+def lu_decomp(matrix):
+    #  Metoda zaczerpnięta z internetu.
+    #  https://www.quantstart.com/articles/LU-Decomposition-in-Python-and-NumPy
+    n = len(matrix)
+    L = [[0.0] * n for _ in range(n)]
+    U = [[0.0] * n for _ in range(n)]
+    P = pivot_mat(matrix)
+    PA = mul_mat_by_mat(P, matrix)
+    for j in range(n):
+        L[j][j] = 1.0
+        for i in range(j + 1):
+            s1 = sum(U[k][j] * L[i][k] for k in range(i))
+            U[i][j] = PA[i][j] - s1
+
+        for i in range(j, n):
+            s2 = sum(U[k][j] * L[i][k] for k in range(j))
+            L[i][j] = (PA[i][j] - s2) / U[j][j]
+
+    return P, L, U
+
+
+def dot_product(v1, v2) -> [float]:
+    return sum([i * j for (i, j) in zip(v1, v2)])
+
+
+def back_sub(upper, b) -> [float]:
+    n = len(upper)
+    r = [0.0 for _ in b]
+    for i in range(n-1, 0, -1):
+        r[i] = (b[i] - dot_product(upper[i], r)) / float(upper[i][i])
+
+    return r
+
+
+def forward_sub(lower, b) -> [float]:
+    n = len(lower)
+    r = [0.0 for _ in b]
+    for i in range(n):
+        r[i] = (b[i] - dot_product(lower[i], r))/lower[i][i]
+
+    return r
+
+
+def mul_mat_by_vec(a, b) -> [[float]]:
+    result = [0.0 for _ in range(len(a))]
+
+    for i in range(len(a)):
+        for k in range(len(b)):
+            result[i] += a[i][k] * b[k]
+
+    return result
+
+
+def sub_vectors(a, b) -> [float]:
+    return [a[i] - b[i] for i in range(len(a))]
+
+
+def norm_res(matrix, r, b) -> float:
+    # norm(M*r - b)
+    # norm = sqrt(sum(|v|[k]^2)
+    # matrix nxn
+    # r nx1
+    # b nx1
+    # v = m*r - b; nx1
+    res = sub_vectors(mul_mat_by_vec(matrix, r), b)
+    ret = sqrt(sum([e ** 2 for e in res]))  # norm
+
+    return ret
+
+
+def prepare(n, a1, a2, a3) -> ([[float]], [float]):
+    matrix = [[0.0 for _ in range(n)] for _ in range(n)]
+
+    for i in range(n):
+        for j in range(n):
+            if i == j:
+                matrix[i][j] = a1
+            elif i == j - 1 or i == j + 1:
+                matrix[i][j] = a2
+            elif i == j - 2 or i == j + 2:
+                matrix[i][j] = a3
+
+    b = [sin(i) for i in range(n)]
+
+    return matrix, b
+
+
+def jacobi(matrix, b, n, cutoff) -> [list, int]:
+    iterations = 0
+    r = [1.0 for _ in range(n)]
+    x = [1.0 for _ in range(n)]
+    try:
+        for iterations in range(1000):
+            accuracy = norm_res(matrix, r, b)
+            if accuracy <= cutoff:
+                return r, iterations
+
+            r = [x[i] for i in range(n)]
+
+            for i in range(n):
+                o = 0
+                for j in range(n):
+                    if i != j:
+                        o += matrix[i][j] * r[j]
+
+                x[i] = (b[i] - o)/matrix[i][i]
+
+    except OverflowError:
+        print("ERROR: OverflowError in Jacobi method, terminating.")
+        return None, iterations
+
+    print("ERROR: too many iterations in Jacobi method, terminating.")
+    print("Current r:", r)
+    return None, iterations
+
+
+def gauss_seidel(matrix, b, n, cutoff) -> [list, int]:
+    iterations = 0
+    r = [1.0 for _ in range(n)]
+
+    try:
+        for iterations in range(1000):
+            accuracy = norm_res(matrix, r, b)
+            if accuracy <= cutoff:
+                return r, iterations
+
+            for i in range(n):
+                o = 0
+                for j in range(n):
+                    if i != j:
+                        o += matrix[i][j] * r[j]
+
+                r[i] = (b[i] - o) / matrix[i][i]
+
+    except OverflowError:
+        print("ERROR: OverflowError in Jacobi method, terminating.")
+        return None, iterations
+
+    print("ERROR: too many iterations in Gauss-Seidel method, terminating.")
+    print("Current r:", r)
+    return None, iterations
+
+
+def gauss(matrix, b):
+    #  Ax = b
+    #  PA = LU
+    #  LUx = Pb
+    #  Ly = Pb
+    #  Ux = y
+    pivot, lower, upper = lu_decomp(matrix)
+    y = back_sub(upper, b)
+    x = forward_sub(lower, y)
+    return x, norm_res(matrix, b, x)
